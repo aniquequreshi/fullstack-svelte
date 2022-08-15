5ab19846afbe624e6a1203031adb08f36fd710e4
Anique Qureshi <anique.qureshi@gmail.com>
2022-08-15 00:11:27 +0000

    Develop HTML Request

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,16 @@
+<div>
+    <form action="">
+        <input type="hidden" name="done" value="">
+        <button>Done/not</button>
+    </form>
+
+    <form action="">
+        <input type="text" name="" id="">
+        <button>Save</button>
+    </form>
+
+    <form action="">
+        <button>Delete</button>
+    </form>
+
+</div>
